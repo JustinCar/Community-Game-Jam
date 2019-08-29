89cf259e33a6beb7efdb4a1534c13d1215f29fa9
JustinCar <carterjustin67@gmail.com>
2019-08-29 18:40:47 +0100

    main menu2

---
+++ Community-Game-Jam/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void quit() 
+    {
+        Application.Quit();
+    }
+
+    public void continueOn() 
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void newGame() 
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
